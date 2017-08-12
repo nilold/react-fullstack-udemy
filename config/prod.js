d2aeb2045cc1a8ea88d336e92174d606401c4ce3
nilo <nilold@gmail.com>
2017-08-12 17:44:20 -0300

    fix error

--- config/prod.js
+++ config/prod.js
@@ -6,8 +6,3 @@ module.exports = {
   mongoURI: 'process.env.MONGO_URI',
   cookieKey: 'process.env.COOKIE_KEY'
 }
-
-
-
-facebookClientID: '113404095990280',
-faceBookClientSecret: 'd3d30213361c9d69e852d5e49376607f',
