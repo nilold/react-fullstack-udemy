05c5ea672c872ca6e0a5c62fb5fd629037c2c315
nilo <nilold@gmail.com>
2017-08-12 18:05:08 -0300

    more debugging

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
   googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
   facebookClientID: 'process.env.FACEBOOK_CLIENT_ID',
   faceBookClientSecret: 'process.env.FACEBOOK_CLIENT_SECRET',
-  mongoURI: 'process.env.MONGO_URI',
-  // mongoURI: 'mongodb://nilo:abacate4@ds153705.mlab.com:53705/emaily-prod',
+  // mongoURI: 'process.env.MONGO_URI',
+  mongoURI: 'mongodb://nilo:abacate4@ds153705.mlab.com:53705/emaily-prod',
   cookieKey: 'process.env.COOKIE_KEY'
 }
