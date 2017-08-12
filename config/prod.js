ac04106b418464f65c9b2e4895969f5ff9a4a43b
nilo <nilold@gmail.com>
2017-08-12 18:07:35 -0300

    more debugging

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,8 @@
 module.exports = {
-  googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-  googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
-  facebookClientID: 'process.env.FACEBOOK_CLIENT_ID',
-  faceBookClientSecret: 'process.env.FACEBOOK_CLIENT_SECRET',
-  // mongoURI: 'process.env.MONGO_URI',
-  mongoURI: 'mongodb://nilo:abacate4@ds153705.mlab.com:53705/emaily-prod',
-  cookieKey: 'process.env.COOKIE_KEY'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  faceBookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 }
