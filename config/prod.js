2a6bd865e65ecdf38a1a55fc4dbb3dbdd09ae387
nilo <nilold@gmail.com>
2017-08-12 17:36:22 -0300

    prod config file added

---
+++ config/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID: 'process.env.GOOGLE_CLIENT_ID',
+  googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
+  facebookClientID: 'process.env.FACEBOOK_CLIENT_ID',
+  faceBookClientSecret: 'process.env.FACEBOOK_CLIENT_SECRET',
+  mongoURI: 'process.env.MONGO_URI',
+  cookieKey: 'process.env.COOKIE_KEY'
+}
+
+
+
+facebookClientID: '113404095990280',
+faceBookClientSecret: 'd3d30213361c9d69e852d5e49376607f',
